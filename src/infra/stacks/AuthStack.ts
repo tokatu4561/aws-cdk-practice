6b0325d46896419a3c5feaa111e58887abe9d65c
tokatu4561 <ryobsk4561@gmail.com>
2024-01-31 23:21:08 +0900

    create role

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -1,19 +1,36 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
-import { CfnIdentityPool, CfnUserPoolGroup, UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito";
+import {
+  CfnIdentityPool,
+  CfnIdentityPoolRoleAttachment,
+  CfnUserPoolGroup,
+  UserPool,
+  UserPoolClient,
+} from "aws-cdk-lib/aws-cognito";
+import {
+  Effect,
+  FederatedPrincipal,
+  PolicyStatement,
+  Role,
+} from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 
 export class AuthStack extends Stack {
   public userPool: UserPool;
   public userPoolClient: UserPoolClient;
   private identityPool: CfnIdentityPool;
+  private authenticatedRole: Role;
+  private unAuthenticatedRole: Role;
+  private adminRole: Role;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     this.createUserPool();
     this.createUserPoolClient();
-    this.createAdminsGroup();
     this.createIdentityPool();
+    this.createRoles();
+    this.attachRoles();
+    this.createAdminsGroup();
   }
 
   private createUserPool() {
@@ -47,9 +64,10 @@ export class AuthStack extends Stack {
 
   private createAdminsGroup() {
     // 管理者グループを作成
-    const adminsGroup = new CfnUserPoolGroup(this, "AdminsGroup", {
+    new CfnUserPoolGroup(this, "AdminsGroup", {
       groupName: "Admins",
       userPoolId: this.userPool.userPoolId,
+      roleArn: this.adminRole.roleArn,
     });
   }
 
@@ -66,6 +84,82 @@ export class AuthStack extends Stack {
     });
     new CfnOutput(this, "SpaceIdentityPoolId", {
       value: this.identityPool.ref,
+    });
+  }
+
+  private createRoles() {
+    // cognito identity pool に対して、認証されたユーザーと認証されていないユーザーのロールを作成
+    // cognito identity pool がロールを引き受けて、ユーザーに代わって AWS サービスにアクセスできるようにする
+    this.authenticatedRole = new Role(this, "CognitoDefaultAuthenticatedRole", {
+      assumedBy: new FederatedPrincipal(
+        "cognito-identity.amazonaws.com",
+        {
+          StringEquals: {
+            "cognito-identity.amazonaws.com:aud": this.identityPool.ref,
+          },
+          "ForAnyValue:StringLike": {
+            "cognito-identity.amazonaws.com:amr": "authenticated",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity" // cognito が引き受けるロールの種類 一時的な認証情報を取得する
+      ),
+    });
+    this.unAuthenticatedRole = new Role(
+      this,
+      "CognitoDefaultUnauthenticatedRole",
+      {
+        assumedBy: new FederatedPrincipal(
+          "cognito-identity.amazonaws.com",
+          {
+            StringEquals: {
+              "cognito-identity.amazonaws.com:aud": this.identityPool.ref,
+            },
+            "ForAnyValue:StringLike": {
+              "cognito-identity.amazonaws.com:amr": "unauthenticated",
+            },
+          },
+          "sts:AssumeRoleWithWebIdentity"
+        ),
+      }
+    );
+    this.adminRole = new Role(this, "CognitoAdminRole", {
+      assumedBy: new FederatedPrincipal(
+        "cognito-identity.amazonaws.com",
+        {
+          StringEquals: {
+            "cognito-identity.amazonaws.com:aud": this.identityPool.ref,
+          },
+          "ForAnyValue:StringLike": {
+            "cognito-identity.amazonaws.com:amr": "authenticated",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity"
+      ),
+    });
+    // s3 へのフルアクセスを許可
+    this.adminRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["s3:*"],
+        resources: ["*"],
+      })
+    );
+  }
+
+  private attachRoles() {
+    new CfnIdentityPoolRoleAttachment(this, "RolesAttachment", {
+      identityPoolId: this.identityPool.ref,
+      roles: {
+        authenticated: this.authenticatedRole.roleArn,
+        unauthenticated: this.unAuthenticatedRole.roleArn,
+      },
+      roleMappings: {
+        adminsMapping: {
+          type: "Token",
+          ambiguousRoleResolution: "AuthenticatedRole",
+          identityProvider: `${this.userPool.userPoolProviderName}:${this.userPoolClient.userPoolClientId}`,
+        },
+      },
     });
   }
 }
