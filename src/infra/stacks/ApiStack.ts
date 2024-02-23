eb52a78b1003478f0ff1766f2a41fede99341b09
tokatu4561 <ryobsk4561@gmail.com>
2024-02-23 23:59:56 +0900

    cors

--- src/infra/stacks/ApiStack.ts
+++ src/infra/stacks/ApiStack.ts
@@ -1,38 +1,71 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
-import { AuthorizationType, CognitoUserPoolsAuthorizer, LambdaIntegration, MethodOptions, RestApi } from 'aws-cdk-lib/aws-apigateway';
-import { IUserPool } from 'aws-cdk-lib/aws-cognito';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  LambdaIntegration,
+  MethodOptions,
+  ResourceOptions,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { IUserPool } from "aws-cdk-lib/aws-cognito";
+import { Construct } from "constructs";
 
 interface ApiStackProps extends StackProps {
-    spacesLambdaIntegration: LambdaIntegration
-    userPool: IUserPool
+  spacesLambdaIntegration: LambdaIntegration;
+  userPool: IUserPool;
 }
 
 export class ApiStack extends Stack {
+  constructor(scope: Construct, id: string, props: ApiStackProps) {
+    super(scope, id, props);
 
-    constructor(scope: Construct, id: string, props: ApiStackProps) {
-        super(scope, id, props);
+    const api = new RestApi(this, "SpacesApi");
 
-        const api = new RestApi(this, 'SpacesApi');
+    // API の認証にCognitoを使用する
+    const authorizer = new CognitoUserPoolsAuthorizer(
+      this,
+      "SpacesAuthorizer",
+      {
+        cognitoUserPools: [props.userPool],
+        identitySource: "method.request.header.Authorization", // Authorizationヘッダーを使用して認証する 認証情報の場所
+      }
+    );
+    authorizer._attachToApi(api);
 
-        // API の認証にCognitoを使用する
-        const authorizer = new CognitoUserPoolsAuthorizer(this, 'SpacesAuthorizer', {
-            cognitoUserPools: [props.userPool],
-            identitySource: 'method.request.header.Authorization', // Authorizationヘッダーを使用して認証する 認証情報の場所
-        });
-        authorizer._attachToApi(api);
+    const optionWithAuthorizer: MethodOptions = {
+      authorizationType: AuthorizationType.COGNITO,
+      authorizer: {
+        authorizerId: authorizer.authorizerId,
+      },
+    };
 
-        const optionWithAuthorizer: MethodOptions = {
-            authorizationType: AuthorizationType.COGNITO,
-            authorizer: {
-                authorizerId: authorizer.authorizerId,
-            },
-        };
+    const optionWithCors: ResourceOptions = {
+      defaultCorsPreflightOptions: {
+        allowOrigins: ["*"],
+        allowMethods: ["GET", "POST", "PUT", "DELETE"],
+      },
+    };
 
-        const spacesResource = api.root.addResource('spaces');
-        spacesResource.addMethod('GET', props.spacesLambdaIntegration, optionWithAuthorizer);
-        spacesResource.addMethod('POST', props.spacesLambdaIntegration, optionWithAuthorizer);
-        spacesResource.addMethod('PUT', props.spacesLambdaIntegration, optionWithAuthorizer);
-        spacesResource.addMethod('DELETE', props.spacesLambdaIntegration, optionWithAuthorizer);
-    }
+    const spacesResource = api.root.addResource("spaces", optionWithCors);
+    spacesResource.addMethod(
+      "GET",
+      props.spacesLambdaIntegration,
+      optionWithAuthorizer
+    );
+    spacesResource.addMethod(
+      "POST",
+      props.spacesLambdaIntegration,
+      optionWithAuthorizer
+    );
+    spacesResource.addMethod(
+      "PUT",
+      props.spacesLambdaIntegration,
+      optionWithAuthorizer
+    );
+    spacesResource.addMethod(
+      "DELETE",
+      props.spacesLambdaIntegration,
+      optionWithAuthorizer
+    );
+  }
 }
