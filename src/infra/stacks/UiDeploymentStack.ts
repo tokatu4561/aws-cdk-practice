daa2a828b647f36e28baf57e375a5b5ded8a0ad2
tokatu4561 <ryobsk4561@gmail.com>
2024-02-23 18:58:40 +0900

    ui deploy

---
+++ src/infra/stacks/UiDeploymentStack.ts
@@ -0,0 +1,55 @@
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, ITable, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import { getSuffixFromStack } from "../Utils";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { join } from "path";
+import { existsSync } from "fs";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+
+export class UiDeploymentStack extends Stack {
+  public readonly spacesTable: ITable;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // ui deployment bucket suffix is the same as the data stack
+    const suffix = getSuffixFromStack(this);
+    const deploymentBucket = new Bucket(this, `uiDeploymentBucket${suffix}`, {
+      bucketName: `frontend-${suffix}`,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const uiDir = join(__dirname, "..", "..", "frontend", "dist");
+    if (!existsSync(uiDir)) {
+      throw new Error(`UI directory not found at ${uiDir}`);
+    }
+
+    new BucketDeployment(this, `uiDeployment${suffix}`, {
+      sources: [Source.asset(uiDir)],
+      destinationBucket: deploymentBucket,
+    });
+
+    // create a origin access identity for the cloudfront distribution
+    const originAccessIdentity = new OriginAccessIdentity(this, "OAI", {
+      comment: `OAI for ${suffix}`,
+    });
+    deploymentBucket.grantRead(originAccessIdentity);
+
+    // distribution
+    const distribution = new Distribution(this, `uiDistribution${suffix}`, {
+      defaultBehavior: {
+        origin: new S3Origin(deploymentBucket, {
+          originAccessIdentity,
+        }),
+      },
+    });
+
+    // output the distribution domain name
+    new CfnOutput(this, "SpaceFinderUrl", {
+      value: distribution.distributionDomainName,
+    });
+  }
+}
