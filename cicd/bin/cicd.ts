a10a93a8edbcda962d296416d38a9d2e9859dd3b
tokatu4561 <ryobsk4561@gmail.com>
2024-04-24 22:26:14 +0900

    add lambda stack stage

--- cicd/bin/cicd.ts
+++ cicd/bin/cicd.ts
@@ -19,3 +19,5 @@ new CicdStack(app, 'CicdStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+app.synth();
--- cicd/lib/cicd-stack.ts
+++ cicd/lib/cicd-stack.ts
@@ -1,13 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'CiCdPipeline', {
+    const pipeline = new CodePipeline(this, 'CiCdPipeline', {
       pipelineName: 'CiCdPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('tokatu4561/aws-cdk-practice', 'main'),
@@ -19,5 +20,9 @@ export class CicdStack extends cdk.Stack {
         primaryOutputDirectory: 'cicd/cdk.out'
       }),
     })
+
+    const devStage = pipeline.addStage(new PipelineStage(this, 'Dev', {
+      stageName: 'Dev'
+    }))
   }
 }
